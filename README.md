738e176a06d5fead1174666e54940aded33c97c2
Ilia Vazyukov <regilaxiomma@gmail.com>
2022-12-14 22:15:55 +0300

    Step 5

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/kilobbo/java-project-61/workflows/hexlet-check/badge.svg)](https://github.com/kilobbo/java-project-61/actions)
+<a href="https://codeclimate.com/github/kilobbo/java-project-61/maintainability"><img src="https://api.codeclima
+te.com/v1/badges/542e174b1ca5d3d84c83/maintainability" /></a>
+
--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,8 +1,22 @@
 package hexlet.code;
 
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Welcome to the Brain Games!");
-        Cli.player();
+        System.out.println("Please enter the game number and press Enter.\n" +
+                "1 - Greet\n2 - Even\n3 - Calc\n4 - GCD\n5 - Progression\n6 - Prime\n0 - Exit\n" +
+                "Your choice: ");
+
+        Scanner scanner = new Scanner(System.in);
+        String choice = scanner.next();
+
+        if (choice.equals("2")) {
+            Even.gameEven();
+        }
+
+        scanner.close();
     }
 }
+
+// остановились на том, что не считывается двойка
