2f929350bbe7678b098c0e7663b686758791bd8a
Ilia Vazyukov <regilaxiomma@gmail.com>
2022-12-18 19:23:09 +0300

    fix 1 - Greet

--- app/src/main/java/hexlet/code/Cli.java
+++ app/src/main/java/hexlet/code/Cli.java
@@ -6,6 +6,7 @@ public class Cli {
     public static void player() {
         Scanner scanner = new Scanner(System.in);
 
+        System.out.println("Welcome to the Brain Games!");
         System.out.print("What is your name? ");
         String userName = scanner.next();
         System.out.println("Hello, " + userName + "!");
