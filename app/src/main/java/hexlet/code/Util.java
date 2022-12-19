5c7a14fa0be54f03dbdd690323f7f9c087d23850
Ilia Vazyukov <regilaxiomma@gmail.com>
2022-12-19 13:16:40 +0300

    add min number to util.getrandomnumber()

--- app/src/main/java/hexlet/code/Util.java
+++ app/src/main/java/hexlet/code/Util.java
@@ -3,9 +3,10 @@ package hexlet.code;
 public class Util {
     public static int getRandomNumber() {
         int maxNumber = 100;
+        int minNumber = 2;
         int randomNumber = (int) Math.floor(Math.random() * maxNumber);
         if (randomNumber == 0) {
-            randomNumber += 1;
+            randomNumber += minNumber;
         }
         return randomNumber;
     }
